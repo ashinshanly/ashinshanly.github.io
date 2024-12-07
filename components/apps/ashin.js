ca61d2a7541626df61b8e17481fe6b41191f644d
Ashin Shanly <ashinkoottala@gmail.com>
2024-12-07 17:11:43 +0530

    Update ashin.js

--- components/apps/ashin.js
+++ components/apps/ashin.js
@@ -69,14 +69,17 @@ export class AboutAshin extends Component {
                     <img className=" w-3 md:w-4" alt="ashin's education" src="./themes/Yaru/status/education.svg" />
                     <span className=" ml-1 md:ml-2 text-gray-50 ">Education</span>
                 </div>
+        
                 <div id="skills" tabIndex="0" onFocus={this.changeScreen} className={(this.state.active_screen === "skills" ? " bg-ub-orange bg-opacity-100 hover:bg-opacity-95" : " hover:bg-gray-50 hover:bg-opacity-5 ") + " w-28 md:w-full md:rounded-none rounded-sm cursor-default outline-none py-1.5 focus:outline-none duration-100 my-0.5 flex justify-start items-center pl-2 md:pl-2.5"}>
                     <img className=" w-3 md:w-4" alt="ashin's skills" src="./themes/Yaru/status/skills.svg" />
                     <span className=" ml-1 md:ml-2 text-gray-50 ">Skills</span>
                 </div>
+        
                 <div id="projects" tabIndex="0" onFocus={this.changeScreen} className={(this.state.active_screen === "projects" ? " bg-ub-orange bg-opacity-100 hover:bg-opacity-95" : " hover:bg-gray-50 hover:bg-opacity-5 ") + " w-28 md:w-full md:rounded-none rounded-sm cursor-default outline-none py-1.5 focus:outline-none duration-100 my-0.5 flex justify-start items-center pl-2 md:pl-2.5"}>
                     <img className=" w-3 md:w-4" alt="ashin's projects" src="./themes/Yaru/status/projects.svg" />
                     <span className=" ml-1 md:ml-2 text-gray-50 ">Projects</span>
                 </div>
+        
                 <div id="resume" tabIndex="0" onFocus={this.changeScreen} className={(this.state.active_screen === "resume" ? " bg-ub-orange bg-opacity-100 hover:bg-opacity-95" : " hover:bg-gray-50 hover:bg-opacity-5 ") + " w-28 md:w-full md:rounded-none rounded-sm cursor-default outline-none py-1.5 focus:outline-none duration-100 my-0.5 flex justify-start items-center pl-2 md:pl-2.5"}>
                     <img className=" w-3 md:w-4" alt="ashin's resume" src="./themes/Yaru/status/download.svg" />
                     <span className=" ml-1 md:ml-2 text-gray-50 ">Resume</span>
@@ -121,7 +124,7 @@ function About() {
                 <img className="w-full" src="./images/logos/bitmoji.png" alt="Ashin Shanly Logo" />
             </div>
             <div className=" mt-4 md:mt-8 text-lg md:text-2xl text-center px-1">
-                <div class="badge-base LI-profile-badge" data-locale="en_US" data-size="large" data-theme="dark" data-type="HORIZONTAL" data-vanity="ashinshanly" data-version="v1"><a class="badge-base__link LI-simple-link" href="https://in.linkedin.com/in/ashinshanly?trk=profile-badge">My name is <span className="font-bold">Ashin Shanly</span></a></div>
+                <div className="font-normal ml-1">My name is <span className="text-pink-600 font-bold">Ashin Shanly</span></div>
                 <div className="font-normal ml-1">I'm a <span className="text-pink-600 font-bold">Software Engineer!</span></div>
             </div>
             <div className=" mt-4 relative md:my-8 pt-px bg-white w-32 md:w-48">
@@ -418,4 +421,3 @@ function Resume() {
         <iframe className="h-full w-full" src="./files/AshinShanly-Resume.pdf" title="ashin shanly resume" frameBorder="0"></iframe>
     )
 }
-<script src="https://platform.linkedin.com/badges/js/profile.js" async defer type="text/javascript"></script>
