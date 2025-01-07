aa1ba8b2a4961598e5d1164f6214288eb5ef07b9
Ashin Shanly <ashinshanly@Ashins-MacBook-Air.local>
2025-01-07 22:57:36 +0530

    Changed chess code

--- components/apps/chess_game.js
+++ components/apps/chess_game.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { db } from '../../config/firebase';
 import { subscribeToGame } from '../../lib/firebase/realtime';
 import { createGame, updateGameState, getGameState } from '../../lib/firebase/database';
-import { initialBoard } from '../../lib/firebase/database';
+//import { initialBoard } from '../../lib/firebase/database';
 
 const initialBoard = [
     ['r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'],
