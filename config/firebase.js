700d71c8fc8bb5c78281eab37eac97e6e80e9072
ashinshanly <ashinkoottala@gmail.com>
2026-03-06 21:45:49 +0530

    refactor: use environment variables for Firebase configuration

--- config/firebase.js
+++ config/firebase.js
@@ -2,14 +2,14 @@ import { initializeApp } from 'firebase/app';
 import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyC3Br7iDX-2irFrN9dLE6vjtsuWFQzfo2U",
-    authDomain: "portfolio-chess.firebaseapp.com",
-    projectId: "portfolio-chess",
-    storageBucket: "portfolio-chess.firebasestorage.app",
-    messagingSenderId: "5157075220",
-    appId: "1:5157075220:web:68efc5fed277db42f8ea01",
-    measurementId: "G-NJVP8F749N"
-  };
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+};
 
 
 // Initialize Firebase
